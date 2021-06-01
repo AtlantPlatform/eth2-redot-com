50b4fc0874d82c6e0245923feaf237fb72bc0833
vlatlant <vl@atlant.io>
2021-06-01 10:30:31 +0300

    fix canonical, footer update

--- handlers/index.go
+++ handlers/index.go
@@ -34,7 +34,7 @@ var indexTemplate = template.Must(template.New("index").Funcs(utils.GetTemplateF
 // Index will return the main "index" page using a go template
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	data := InitPageData(w, r, "index", "", "Index")
+	data := InitPageData(w, r, "index", "/", "Index")
 	data.Data = services.LatestIndexPageData()
 
 	data.Data.(*types.IndexPageData).ShowSyncingMessage = data.ShowSyncingMessage
